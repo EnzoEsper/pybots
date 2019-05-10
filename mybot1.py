8566aadfe425468596a233fb8b8d2f560cceda18
EnzoEsper <enzoesper@hotmail.com>
2019-05-10 17:30:34 -0300

    first simple python bot that tweet a simple line

---
+++ mybot1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Twitter Bot Starter Kit: Bot 1
+
+# This is the most basic bot. It sends out a tweet!
+
+# If you haven't yet created credentials.py, modify credentials.template 
+# to include your own Twitter account settings. This script will then tweet
+# using your bot's account 
+
+# Housekeeping: do not edit
+import tweepy
+from credentials import *
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+# What the bot will tweet
+tweet = 'Hello ALA!'
+
+# Send out the tweet and print it to the console 
+api.update_status(status=tweet)
+print(tweet)
+
+print("All done!")
